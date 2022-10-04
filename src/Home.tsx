01abe8854f4c8042de80ca48beaa423369c2094b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-10-03 23:21:02 -0300

    chore: add husky and prettier configs

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
 export const Home = () => {
   return <Text>Hello</Text>;
