89448b686922b53a709642e480818e9ad0d5684c
Wellington <wellingtonrufino@lelabs.com.br>
2023-03-21 22:23:51 -0300

    feat: header

--- src/Home.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-export const Home = () => {
-  return <Text>Hello</Text>;
-};
--- src/feature/home/screens/Home/Home.view.tsx
+++ src/feature/home/screens/Home/Home.view.tsx
@@ -1,6 +1,45 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+import { Icon } from "@/components";
+import { colors } from "@/styles/colors";
+
+import { styles } from "./styles";
 export const HomeView = () => {
-  return <Text>Hello, world</Text>;
+  return (
+    <SafeAreaView>
+      <View style={styles.headerContainer}>
+        <View style={styles.headerTopContent}>
+          <Text style={styles.userLabel}>
+            Hello, <Text style={styles.userLabelHighlited}> Helena Santos</Text>{" "}
+            👋
+          </Text>
+
+          <View style={styles.iconsContainer}>
+            <TouchableOpacity style={styles.iconWrapper}>
+              <Icon color="white" size={20} name="Bell" />
+            </TouchableOpacity>
+            <TouchableOpacity style={{ ...styles.iconWrapper, marginLeft: 16 }}>
+              <Icon color="white" size={20} name="Engine" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.balanceLabel}>Seu saldo atual</Text>
+
+          <View style={styles.balanceWrapper}>
+            <Text style={styles.balanceCoinLabel}>
+              R$ <Text style={styles.balanceValue}>3.000,00</Text>
+            </Text>
+            <TouchableOpacity style={{ marginLeft: 18 }}>
+              <Icon color={colors.secundaryDark} size={20} name="Eye" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
